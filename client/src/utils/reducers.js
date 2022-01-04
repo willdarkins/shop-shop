978b720acfe8642389021b88fd03f408aa1d315d
Will Darkins <willdarkins@gmail.com>
2022-01-03 23:56:13 -0800

    also tested UPDATE_CATEGORIES and it works

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -4,20 +4,24 @@ import {
     UPDATE_CURRENT_CATEGORY
   } from "./actions";
   
-  export const reducer = (state, action) => {
-    switch (action.type) {
-      // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
-      case UPDATE_PRODUCTS:
-          //If it's that action type, we return a new object with a copy of the state argument using the spread ... operator
-          //then set the products key to a value of a new array with the action.products value spread across it.
-        return {
-          ...state,
-          products: [...action.products],
-        };
+export const reducer = (state, action) => {
+  switch (action.type) {
+    // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
+    case UPDATE_PRODUCTS:
+      return {
+        ...state,
+        products: [...action.products]
+      };
+    // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories]
+      };
+
+    default:
+      return state;
+  }
+};
+
   
-      // if it's none of these actions, do not update state at all and keep things the same!
-      // This is in case we accidentally execute an action that isn't predefined
-      default:
-        return state;
-    }
-  };
