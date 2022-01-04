b5b460aa98232351c0386c546cddfa9b9def5b17
Will Darkins <willdarkins@gmail.com>
2022-01-03 23:50:01 -0800

    first test passed for UPDATED_PRODUCTS

--- client/src/_tests_/reducers.test.js
+++ client/src/_tests_/reducers.test.js
@@ -4,6 +4,8 @@ import {
     UPDATE_CURRENT_CATEGORY
   } from '../utils/actions'
 
+  import { reducer } from '../utils/reducers';
+
 // create a sample of what our global state will look like
 const initialState = {
     products: [],
