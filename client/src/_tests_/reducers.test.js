33cadd36e3a25c4729872404189c57043da2ee13
Will Darkins <willdarkins@gmail.com>
2022-01-03 23:43:58 -0800

    wrote first reducer test to see if by updating the products array, adding two new objects, the initial state would be affected

--- client/src/_tests_/reducers.test.js
+++ client/src/_tests_/reducers.test.js
@@ -11,9 +11,13 @@ const initialState = {
     currentCategory: '1',
   };
 
+//First, we'll write a test for updating that products list...
+    //we'll be testing the UPDATE_PRODUCTS action to see if we can add a product to the products array.
   test('UPDATE_PRODUCTS', () => {
     let newState = reducer(initialState, {
+    //This is the type of action we're performing, and should be one of the predefined actions we created earlier.
       type: UPDATE_PRODUCTS,
+      //This won't always have the name value, but it is a name representative of the new data we want to use with the action
       products: [{}, {}]
     });
   
